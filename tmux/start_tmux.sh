a9155d10e9648a2aad3ed7c3020c4e25a3e727bc
Johnny Zhou <johnnyzhou@slightech.com>
2018-01-16 10:41:29 +0800

    optimize

--- tmux/start_tmux.sh
+++ tmux/start_tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#cd /home/slam/tmp/mydev_env/tmux && sh start_tmux.sh  zhp
 
 tmux attach -t $1
 if [ $? -ne 0 ];then
-	tmux -2 new-session -s $1
+	tmux -2 -f .tmux.conf new-session -s $1
 fi
